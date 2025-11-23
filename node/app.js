8d1989a774ec5140d856de6157cd68e2aff801b9
JorgeChicana <desarrollochicana@gmail.com>
2025-11-23 15:26:34 -0500

    Laboratorio de contenedores con node.js

--- node/app.js
+++ node/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hola desde Node.js dentro de un contenedor Docker");
 });
 
-app.listen(3000, () => {
-    console.log("Servidor ejecutándose en http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor ejecutándose en puerto " + PORT);
 });
+
